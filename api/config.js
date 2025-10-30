67eb6ef5a538db1b1d9f6df64128574611e5d72e
Thomas <thomas.dosne@gmail.com>
2025-10-30 15:50:52 +0100

    credentials

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41mva3uibrs73aoc2ug-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "pollution_dse_user",
+    "password" : "7D2NNdVSdDsQxXutcXt6PpJqBXUDTqZV",
+    "bdname" :"pollution_dse" 
     }
 }
 
