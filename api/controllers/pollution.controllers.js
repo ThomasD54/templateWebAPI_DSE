c0aa04a7dd0f0bde430981cda5b8fc91df940161
Thomas <thomas.dosne@gmail.com>
2025-11-04 22:37:54 +0100

    ajout route

--- api/controllers/pollution.controllers.js
+++ api/controllers/pollution.controllers.js
@@ -4,6 +4,7 @@ const db = require("../models");
 const Pollution = db.pollution;
 const Op = db.Sequelize.Op;
 
+// Obtenir toutes les pollutions :
 exports.get = (req, res) => {
 
      Pollution.findAll()
@@ -13,5 +14,42 @@ exports.get = (req, res) => {
         message: err.message
       });
     });
-
 }; 
+
+// Obtenir une pollution grâce à l'ID :
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+    Pollution.findByPk(id)
+        .then(data => {
+            if (!data) {
+                return res.status(404).send({
+                    message: "Pollution not found"
+                });
+            }
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(400).send({
+                message: err.message
+            });
+        });
+};
+
+// Création d'une nouvelle pollution
+exports.create = (req, res) => {
+    const pollution = {
+        id: uuidv4(),
+        titre: req.body.titre,
+        type_pollution: req.body.type_pollution,
+        description: req.body.description,
+        date_observation: req.body.date_observation,
+        lieu: req.body.lieu,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        photo_url: req.body.photo_url
+    };
+
+    Pollution.create(pollution)
+        .then(data => res.send(data))
+        .catch(err => res.status(400).send({ message: err.message }));
+    };
--- api/routes/pollution.routes.js
+++ api/routes/pollution.routes.js
@@ -1,11 +1,13 @@
 module.exports = app => {
     const pollution = require("../controllers/pollution.controllers.js");
   
-    var router = require("express").Router();
+    const router = require("express").Router();
+  
+    
+    router.get("/", pollution.get);
+    router.get("/:id", pollution.findOne);
+    router.post("/", pollution.create);
   
 
-   
-    router.get("/", pollution.get);
-  
     app.use('/api/pollution', router);
   };
