fe35ab0eac12dd81d2c85a0bb2db7f6ad9f4b369
Thomas <thomas.dosne@gmail.com>
2025-11-11 16:12:12 +0100

    modification de l'API

--- api/controllers/pollution.controllers.js
+++ api/controllers/pollution.controllers.js
@@ -1,5 +1,3 @@
-const { v4: uuidv4 } = require ("uuid");
-
 const db = require("../models");
 const Pollution = db.pollution;
 const Op = db.Sequelize.Op;
@@ -38,7 +36,6 @@ exports.findOne = (req, res) => {
 // Création d'une nouvelle pollution
 exports.create = (req, res) => {
   const pollution = {
-    id: uuidv4(),
     titre: req.body.titre,
     type_pollution: req.body.type_pollution,
     description: req.body.description,
@@ -52,4 +49,54 @@ exports.create = (req, res) => {
   Pollution.create(pollution)
     .then(data => res.send(data))
     .catch(err => res.status(400).send({ message: err.message }));
+};
+
+// Mettre à jour une pollution
+exports.update = (req, res) => {
+  const id = req.params.id;
+  
+  Pollution.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Pollution was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update Pollution with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(400).send({
+        message: "Error updating Pollution with id=" + id
+      });
+    });
+};
+
+// Supprimer une pollution
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Pollution.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Pollution was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Pollution with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(400).send({
+        message: "Could not delete Pollution with id=" + id
+      });
+    });
 };
--- api/routes/pollution.routes.js
+++ api/routes/pollution.routes.js
@@ -7,6 +7,8 @@ module.exports = app => {
     router.get("/", pollution.get);
     router.get("/:id", pollution.findOne);
     router.post("/", pollution.create);
+    router.put("/:id", pollution.update);
+    router.delete("/:id", pollution.delete);
   
 
     app.use('/api/pollution', router);
