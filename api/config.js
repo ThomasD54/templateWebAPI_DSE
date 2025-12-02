d0b4821ad0b9307b02c4418e948e6f608371f445
Thomas <thomas.dosne@gmail.com>
2025-12-02 22:44:35 +0100

    Nouvelle BDD redner

--- api/config.js
+++ api/config.js
@@ -1,12 +1,10 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d41mva3uibrs73aoc2ug-a.oregon-postgres.render.com",
+    "host" :"dpg-d4majh7diees739ukang-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "pollution_dse_user",
-    "password" : "7D2NNdVSdDsQxXutcXt6PpJqBXUDTqZV",
-    "bdname" :"pollution_dse" 
-    // dfsdfsdfsfd
+    "user" : "pollution_dse_v2_user",
+    "password" : "m6T631bQhywma4h3ZYDaGSLziBTTiqJE",
+    "bdname" :"pollution_dse_v2" 
     }
 }
-
