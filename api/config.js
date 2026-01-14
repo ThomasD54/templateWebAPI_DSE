f963be9abfd1a8421760c474f47a8a91125764be
Thomas <thomas.dosne@gmail.com>
2026-01-14 14:31:13 +0100

    mise à jour bdd

--- api/config.js
+++ api/config.js
@@ -1,10 +1,10 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d4majh7diees739ukang-a.oregon-postgres.render.com",
+    "host" :"dpg-d5jpgiffte5s738qvn3g-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "pollution_dse_v2_user",
-    "password" : "m6T631bQhywma4h3ZYDaGSLziBTTiqJE",
-    "bdname" :"pollution_dse_v2" 
+    "user" : "pollution_dse_v3_user",
+    "password" : "D7nz0pWEzoe3oT6y83Xi3pGTjNKSL8sJ",
+    "bdname" :"pollution_dse_v3" 
     }
 }
